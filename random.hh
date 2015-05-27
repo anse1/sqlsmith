baa3702b00c12d2ad15cc12cf39d3edc78b8a8a1
Andreas Seltenreich <andreas+git@ansel.ydns.eu>
2015-05-27 20:52:33 +0200

    Add another random_pick vairant.

--- random.hh
+++ random.hh
@@ -2,6 +2,7 @@
 #define RANDOM_HH
 
 #include <random>
+#include  <iterator>
 
 namespace smith {
   extern std::default_random_engine rng;
@@ -10,6 +11,13 @@ namespace smith {
 template<typename T> T& random_pick(std::vector<T>& container) {
   std::uniform_int_distribution<int> pick(0, container.size()-1);
   return container[pick(smith::rng)];
+}
+
+template<typename I>
+I random_pick(I beg, I end) {
+    std::uniform_int_distribution<> pick(0, std::distance(beg, end) - 1);
+    std::advance(beg, pick(smith::rng));
+    return beg;
 }
 
 #endif
