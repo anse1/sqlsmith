0d0e4dce4d895d7d7ef828d4e03ece3f2c6754b2
Andreas Seltenreich <seltenreich@gmx.de>
2015-06-06 12:51:34 +0200

    Add some views to log schema.

--- log.sql
+++ log.sql
@@ -10,3 +10,9 @@ create table error (
     client inet default inet_client_addr(),
     t timestamptz default now()
 );
+
+create view base_error as
+       select rev, (regexp_split_to_array(msg,'\n'))[1] as error, query from error;
+
+create view report as
+       select count(1), error from base_error group by 2 order by count desc;
