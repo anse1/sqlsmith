0866e0edd5d62913651d4fac21513e7c6d584924
Andreas Seltenreich <seltenreich@gmx.de>
2015-06-06 14:35:50 +0200

    Add instance_activity view to log schema.

--- log.sql
+++ log.sql
@@ -28,9 +28,17 @@ comment on table error is 'observed errors';
 create view base_error as
        select id, (regexp_split_to_array(msg,'\n'))[1] as error, query from error;
 
-comment on view base_error is 'truncate errors to first line for grouping';
+comment on view base_error is 'like error, but truncate msg to first line';
 
 create view report as
        select count(1), error from base_error group by 2 order by count desc;
 
 comment on view report is 'same report as sqlsmith''s verbose output';
+
+create view instance_activity as
+       select hostname, target, max(e.t)
+       	      from instance i join error e on (i.id = e.id)
+	      group by hostname, target
+              order by max desc;
+
+comment on view instance_activity is 'time of last error message from instance';
