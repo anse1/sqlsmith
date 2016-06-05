1d681a3f1cb567ddb66697ec01208fae4b6eb7c2
Andreas Seltenreich <seltenreich@gmx.de>
2016-06-05 19:48:03 +0200

    grammar: Reduce maximum retries.

--- prod.cc
+++ prod.cc
@@ -24,7 +24,7 @@ void prod::indent(std::ostream &out)
 
 void prod::retry()
 {
-  if (retries++ > 100000)
+  if (retries++ > 10000)
     throw std::runtime_error(std::string("excessive retries in ")
 			     + typeid(*this).name());
 }
