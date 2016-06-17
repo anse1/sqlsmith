48f62794704519a7ecf66ee63e9c6eae1c5c3a1c
Andreas Seltenreich <seltenreich@gmx.de>
2016-06-17 22:49:17 +0200

    sqlite: Use flags SQLITE_OPEN_READWRITE|SQLITE_OPEN_URI.

--- sqlite.cc
+++ sqlite.cc
@@ -58,7 +58,7 @@ extern "C" int column_callback(void *arg, int argc, char **argv, char **azColNam
 schema_sqlite::schema_sqlite(std::string &conninfo)
 {
   version = "SQLite " SQLITE_VERSION " " SQLITE_SOURCE_ID;
-  rc = sqlite3_open_v2(conninfo.c_str(), &db, SQLITE_OPEN_READONLY, 0);
+  rc = sqlite3_open_v2(conninfo.c_str(), &db, SQLITE_OPEN_READWRITE|SQLITE_OPEN_URI, 0);
   if (rc) {
     throw std::runtime_error(sqlite3_errmsg(db));
   }
@@ -239,7 +239,7 @@ schema_sqlite::~schema_sqlite()
 
 dut_sqlite::dut_sqlite(std::string &conninfo)
 {
-  rc = sqlite3_open_v2(conninfo.c_str(), &db, SQLITE_OPEN_READONLY, 0);
+  rc = sqlite3_open_v2(conninfo.c_str(), &db, SQLITE_OPEN_URI|SQLITE_OPEN_READWRITE, 0);
   if (rc) {
     throw std::runtime_error(sqlite3_errmsg(db));
   }
