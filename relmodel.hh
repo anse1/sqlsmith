f8a1a293091e347a096237d1021130cc3831f377
Andreas Seltenreich <seltenreich@gmx.de>
2016-07-17 03:17:13 +0200

    relmodel: Comment new sqltype method.

--- relmodel.hh
+++ relmodel.hh
@@ -22,6 +22,15 @@ struct sqltype {
   static map<string, struct sqltype*> typemap;
   static struct sqltype *get(string s);
   sqltype(string n) : name(n) { }
+
+  /* This function is used to model postgres-style pseudotypes.
+     A generic type is consistent with a more concrete type.
+     E.G., anyarray->consistent(intarray) is true
+           while int4array->consistent(anyarray) is false
+
+     There must not be cycles in the consistency graph, since the
+     grammar will use fixpoint iteration to resolve type conformance
+     situations in the direction of more concrete types  */
   virtual bool consistent(struct sqltype *rvalue);
 };
 
