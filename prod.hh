7109b24ccc3dc27a15b1e6026be779683f9b5ca8
Andreas Seltenreich <seltenreich@gmx.de>
2016-07-24 19:52:51 +0200

    Add comments.

--- prod.hh
+++ prod.hh
@@ -15,17 +15,34 @@ struct prod_visitor {
 
 /// Base class for AST nodes
 struct prod {
+  /// Parent production that instanciated this one.  May be NULL for
+  /// top-level productions.
   struct prod *pprod;
+  /// Scope object to model column/table reference visibility.
   struct scope *scope;
+  /// Level of this production in the AST.  0 for root node.
   int level;
+  /// Number of retries in this production.  Child productions are
+  /// generated speculatively and may fail.
   long retries = 0;
+  /// Maximum number of retries allowed before reporting a failure to
+  /// the Parent prod.
   long retry_limit = 200;
   prod(prod *parent);
+  /// Newline and indent according to tree level.
   virtual void indent(std::ostream &out);
+  /// Emit SQL for this production.
   virtual void out(std::ostream &out) = 0;
+  /// Check with the impedance matching code whether this production
+  /// has been blacklisted and throw an exception.
   virtual void match();
+  /// Visitor pattern for walking the AST.  Make sure you visit all
+  /// child production when deriving classes.
   virtual void accept(prod_visitor *v) { v->visit(this); }
+  /// Report a "failed to generate" error.
   virtual void fail(const char *reason);
+  /// Increase the retry count and throw an exception when retry_limit
+  /// is exceeded.
   void retry();
 };
 
--- relmodel.hh
+++ relmodel.hh
@@ -23,14 +23,14 @@ struct sqltype {
   static struct sqltype *get(string s);
   sqltype(string n) : name(n) { }
 
-  /* This function is used to model postgres-style pseudotypes.
-     A generic type is consistent with a more concrete type.
-     E.G., anyarray->consistent(intarray) is true
-           while int4array->consistent(anyarray) is false
+  /** This function is used to model postgres-style pseudotypes.
+      A generic type is consistent with a more concrete type.
+      E.G., anyarray->consistent(intarray) is true
+            while int4array->consistent(anyarray) is false
 
-     There must not be cycles in the consistency graph, since the
-     grammar will use fixpoint iteration to resolve type conformance
-     situations in the direction of more concrete types  */
+      There must not be cycles in the consistency graph, since the
+      grammar will use fixpoint iteration to resolve type conformance
+      situations in the direction of more concrete types  */
   virtual bool consistent(struct sqltype *rvalue);
 };
 
