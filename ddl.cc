7ce7f4d70545dde5fa911ab437a395c8afbf5d70
Andreas Seltenreich <seltenreich@gmx.de>
2018-06-07 17:43:40 +0200

    ddl: Add column_definition.

--- ddl.cc
+++ ddl.cc
@@ -1,9 +1,15 @@
 #include "ddl.hh"
+#include "relmodel.hh"
 
 table_definition::table_definition(prod *parent, struct scope *s)
-     : prod(parent), created_table(s->stmt_uid("new_tab"), "public", true, true)
+     : prod(parent),
+       created_table(s->stmt_uid("new_tab"), "public", true, true)
 {
+     scope = s;
 
+     while (d6() > 1) {
+	  column_defs.push_back(std::make_shared<column_definition>(this));
+     }
 }
 
 void table_definition::out(std::ostream &out)
@@ -11,5 +17,27 @@ void table_definition::out(std::ostream &out)
      out << "create table ";
      out << created_table.ident();
      out << "(";
+
+
+     for (auto defn = column_defs.begin(); defn != column_defs.end(); defn++) {
+	  out << **defn;
+	  if (defn+1 != column_defs.end())
+	       out << ",", indent(out);
+     }
+
      out << ")";
+}
+
+column_definition::column_definition(prod *parent)
+     : prod(parent)
+{
+     created_column = std::make_shared<column>(
+	  scope->stmt_uid("c"),
+	  random_pick(scope->schema->types)
+	  );
+}
+							  
+void column_definition::out(std::ostream &out)
+{
+     out << created_column->name << " " << created_column->type->name;
 }
--- ddl.hh
+++ ddl.hh
@@ -4,15 +4,30 @@
 #ifndef DDL_HH
 #define DDL_HH
 
+#include <memory>
+
 #include "prod.hh"
 #include "relmodel.hh"
 #include "grammar.hh"
 
+struct column_definition : prod {
+     shared_ptr<column> created_column;
+     column_definition(prod *parent);
+     virtual void out(std::ostream &out);
+     virtual ~column_definition() {  };
+};
+
 struct table_definition : prod {
      table created_table;
+     vector<shared_ptr<column_definition> > column_defs;
      table_definition(prod *parent, struct scope *s);
      virtual void out(std::ostream &out);
      virtual ~table_definition() {  };
+     virtual void accept(prod_visitor *v) {
+	  for (auto el : column_defs)
+	       el->accept(v);
+	  v->visit(this);
+     }
 };
 
 #endif
