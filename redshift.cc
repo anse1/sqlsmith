40b9b1228103bd72d4a0aa866a9a817b0626f823
Robins Tharakan <tharakan@gmail.com>
2019-11-17 10:05:05 +0000

    Redshift is based off of v8.0.2 and didn't have Windowing functions. Also we can't do a server_version_num although that doesn't matter since Redshift doesn't need a version check since we already know how to check for aggregates (proisagg)

--- redshift.cc
+++ redshift.cc
@@ -99,13 +99,6 @@ schema_redshift::schema_redshift(std::string &conninfo, bool no_catalog) : c(con
   pqxx::result r = w.exec("select version()");
   version = r[0][0].as<string>();
 
-  r = w.exec("SHOW server_version_num");
-  version_num = r[0][0].as<int>();
-
-  // address the schema change in postgresql 11 that replaced proisagg and proiswindow with prokind
-  string procedure_is_aggregate = version_num < 110000 ? "proisagg" : "prokind = 'a'";
-  string procedure_is_window = version_num < 110000 ? "proiswindow" : "prokind = 'w'";
-
   cerr << "Loading types...";
 
   r = w.exec("select quote_ident(typname), oid, typdelim, typrelid, typelem, typarray, typtype "
@@ -216,7 +209,7 @@ schema_redshift::schema_redshift(std::string &conninfo, bool no_catalog) : c(con
 	     "and proname <> 'pg_event_trigger_table_rewrite_reason' "
 	     "and proname <> 'pg_event_trigger_table_rewrite_oid' "
 	     "and proname !~ '^ri_fkey_' "
-	     "and not (proretset or " + procedure_is_aggregate + " or " + procedure_is_window + ") ");
+	     "and not (proretset or proisagg) ");
 
   for (auto row : r) {
     routine proc(row[0].as<string>(),
@@ -252,8 +245,8 @@ schema_redshift::schema_redshift(std::string &conninfo, bool no_catalog) : c(con
 	     "and proname not in ('percentile_cont', 'dense_rank', 'cume_dist', "
 	     "'rank', 'test_rank', 'percent_rank', 'percentile_disc', 'mode', 'test_percentile_disc') "
 	     "and proname !~ '^ri_fkey_' "
-	     "and not (proretset or " + procedure_is_window + ") "
-	     "and " + procedure_is_aggregate);
+	     "and not proretset "
+	     "and proisagg");
 
   for (auto row : r) {
     routine proc(row[0].as<string>(),
